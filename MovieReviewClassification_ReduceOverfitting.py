0cca1e51f6738db038bd4b5e3dcd98298929cdd3
wmtowne <wmtowne@svsu.edu>
2022-05-22 17:44:48 -0400

    Finished chapter 4

--- MovieReviewClassification_ReduceOverfitting.py
+++ MovieReviewClassification_ReduceOverfitting.py
@@ -1,5 +1,6 @@
 from keras import models
 from keras import layers
+from keras import regularizers
 
 # Original network
 # model = models.Sequential()
@@ -8,7 +9,37 @@ from keras import layers
 # model.add(layers.Dense(1, activation='sigmoid'))
 
 # Smaller network (wtih lower capacity)
+# This smaller network ensures that
+# *******************************************************************
+# model = models.Sequential()
+# model.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
+# model.add(layers.Dense(4, activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+# *******************************************************************
+
+# Larger network (with larger capacity)
+# model = models.Sequential()
+# model.add(layers.Dense(512, activation='relu', input_shape=(10000,)))
+# model.add(layers.Dense(512, activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+
+# Original network with regularizers
+# model = models.Sequential()
+# model.add(layers.Dense(16, kernel_regularizer=regularizers.l2(0.001),
+#                       activation='relu', input_shape=(10000,)))
+# model.add(layers.Dense(16, kernel_regularizer=regularizers.l2(0.001),
+#                      activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+
+# Original network with dropout
 model = models.Sequential()
-model.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(4, activation='relu'))
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation='sigmoid'))
+
+# Takeaways/Follow ups
+# 1.) Study up on l1 and l4 regularization again
+# 2.) Study up on some of the activiation and loss functions
+
